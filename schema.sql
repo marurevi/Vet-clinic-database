ac7fdf31e1e70d18ac1a678832b574e45db3d2ca
marurevi <maryrev@hotmail.com>
2022-07-05 19:41:34 -0300

    Modify weight of animals

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- /* Database schema to keep the structure of entire database. */
+/* Database schema to keep the structure of entire database. */
 -- CREATE TABLE animals (
 --     id INT GENERATED ALWAYS AS IDENTITY,
 --     name VARCHAR(50),
@@ -12,12 +12,12 @@
 -- ALTER TABLE animals
 -- ADD species VARCHAR(50);
 
+-- BEGIN TRANSACTION;
 -- UPDATE animals
 -- SET species= 'unspecified';
+-- ROLLBACK;
 
--- UPDATE animals
--- SET species= NULL;
-
+-- BEGIN TRANSACTION;
 -- UPDATE animals
 -- SET species= 'digimon'
 -- WHERE name LIKE '%mon';
@@ -25,7 +25,19 @@
 -- UPDATE animals
 -- SET species= 'pokemon'
 -- WHERE species ISNULL;
+-- COMMIT TRANSACTION;
 
+
+-- BEGIN TRANSACTION;
 -- DELETE FROM animals;
+-- ROLLBACK;
 
+BEGIN TRANSACTION;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg= weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg= weight_kg* -1 WHERE weight_kg < 0;
+COMMIT TRANSACTION;
+
+
