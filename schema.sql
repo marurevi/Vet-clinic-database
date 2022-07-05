f7fb6b4af1019fc6a25cc8039481a9198fbe16cc
marurevi <maryrev@hotmail.com>
2022-07-05 15:53:59 -0300

    Add species of type string

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
-/* Database schema to keep the structure of entire database. */
-CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(50),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL,
-    PRIMARY KEY (id)
-);
+-- /* Database schema to keep the structure of entire database. */
+-- CREATE TABLE animals (
+--     id INT GENERATED ALWAYS AS IDENTITY,
+--     name VARCHAR(50),
+--     date_of_birth DATE,
+--     escape_attempts INT,
+--     neutered BOOLEAN,
+--     weight_kg DECIMAL,
+--     PRIMARY KEY (id)
+-- );
+
+ALTER TABLE animals
+ADD species VARCHAR(50);
