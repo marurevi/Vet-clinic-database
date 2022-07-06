da24f505a9718d4764b927ab61b0ea6a995cc221
marurevi <maryrev@hotmail.com>
2022-07-05 21:20:20 -0300

    Add Queries

--- queries.sql
+++ queries.sql
@@ -23,3 +23,23 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE 10.4 <= weight_kg AND weight_kg <= 17.3;
+
+///* SECOND PULL REQUEST *///
+
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts= 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, SUM(escape_attempts) AS WhoEscapeMost FROM animals GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg) AS minWgth, MAX(weight_kg) AS maxWgth FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -1,36 +1,35 @@
 /* Database schema to keep the structure of entire database. */
--- CREATE TABLE animals (
---     id INT GENERATED ALWAYS AS IDENTITY,
---     name VARCHAR(50),
---     date_of_birth DATE,
---     escape_attempts INT,
---     neutered BOOLEAN,
---     weight_kg DECIMAL,
---     PRIMARY KEY (id)
--- );
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    PRIMARY KEY (id)
+);
 
--- ALTER TABLE animals
--- ADD species VARCHAR(50);
+ALTER TABLE animals
+ADD species VARCHAR(50);
 
--- BEGIN TRANSACTION;
--- UPDATE animals
--- SET species= 'unspecified';
--- ROLLBACK;
+BEGIN TRANSACTION;
+UPDATE animals
+SET species= 'unspecified';
+ROLLBACK;
 
--- BEGIN TRANSACTION;
--- UPDATE animals
--- SET species= 'digimon'
--- WHERE name LIKE '%mon';
+BEGIN TRANSACTION;
+UPDATE animals
+SET species= 'digimon'
+WHERE name LIKE '%mon';
 
--- UPDATE animals
--- SET species= 'pokemon'
--- WHERE species ISNULL;
--- COMMIT TRANSACTION;
+UPDATE animals
+SET species= 'pokemon'
+WHERE species ISNULL;
+COMMIT TRANSACTION;
 
-
--- BEGIN TRANSACTION;
--- DELETE FROM animals;
--- ROLLBACK;
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
 
 BEGIN TRANSACTION;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
