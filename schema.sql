765ae98a925898771684474b24620b23cd26a98f
marurevi <maryrev@hotmail.com>
2022-07-06 16:49:11 -0300

    Create Species and Owner Tables Modify animals table deleting species column and adding species_id and 	owner_id related wich those tables

--- schema.sql
+++ schema.sql
@@ -39,4 +39,33 @@ ROLLBACK TO SP1;
 UPDATE animals SET weight_kg= weight_kg* -1 WHERE weight_kg < 0;
 COMMIT TRANSACTION;
 
+/* THIRD PULL REQUEST */
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(50),
+    age INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    PRIMARY KEY (id)
+);
+
+-- Modify animals table:
+--Remove column species
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ADD COLUMN species_id INT, 
+ADD FOREIGN KEY(species_id) REFERENCES species(id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals 
+ADD COLUMN owner_id INT, 
+ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
+
 
